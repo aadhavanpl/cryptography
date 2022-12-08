3a35e583bf564511f8410cd99f496534e0c27858
AadhavanPL <aadhavanlenin@gmail.com>
2022-12-08 11:49:40 +0530

    vignere done

--- vignere.py
+++ vignere.py
@@ -4,9 +4,17 @@ def letterToNumber(s):
         num_list.append(str((ord(c) - 65)))
     return num_list
 
-
 cipherInput ="IYEGXPXMMOETTNZKAYAUYYTTBBMVFFYYKKKCIGXELZFXBAVSIJZOXQBBNXUWIOYZVTFRVABXMZXWYROXAZRTXOEOLSKUTJAEKMGWABWHVAMYKPHQVCQLFMQNWOEOBRMETXOFVTQMGHJIIGIRWTKEVYQVFIFAJAEKMGWALYYVAVMUCKIYJQHLNHGGZZWGQBUTXGIMFYLRYVUDAVPIGVL"
 cipherText = letterToNumber(cipherInput)
 
-key = [0] * 10
+givenKey = "INTEGRITY"
+key = letterToNumber(givenKey)
 
+plainText = ''
+j = 0
+
+for i in range(len(cipherText)):
+    plainText+=chr((int(cipherText[i]) - int(key[j]))%26 + 65)
+    j+=1
+    if j == len(key): j = 0
+print(plainText)
